fa9ffcfaa2bf1dcefcb9c77e8903b94013a3d3c9
MAC <mac@git.axolotl.ft.com>
2026-04-21 22:57:51 -0600

    fix: corregir useModalFormConfirm para usar requestSubmit

--- parabolic-lab-frontend/src/hooks/useModalFormConfirm.ts
+++ parabolic-lab-frontend/src/hooks/useModalFormConfirm.ts
@@ -1,13 +1,9 @@
 import { useRef } from "react";
-
 const useModalFormConfirm = () => {
   const formRef = useRef<HTMLFormElement>(null);
-
   const handleConfirmClick = () => {
-    formRef.current?.submit();
+    formRef.current?.requestSubmit();
   };
-
   return { formRef, handleConfirmClick };
 };
-
 export default useModalFormConfirm;
